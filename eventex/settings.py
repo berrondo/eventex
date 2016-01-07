4d309593988b356dd796313e4d6fd21fdbac607a
Claudio Berrondo <claudio.berrondo@gmail.com>
2016-01-07 00:23:21 -0200

    formulario de inscricao

--- eventex/settings.py
+++ eventex/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'eventex.core'
+    'eventex.core',
+    'eventex.subscriptions',
 ]
 
 MIDDLEWARE_CLASSES = [
--- eventex/urls.py
+++ eventex/urls.py
@@ -18,8 +18,10 @@ from django.conf.urls import url
 from django.contrib import admin
 
 from eventex.core.views import home
+from eventex.subscriptions.views import subscribe
 
 urlpatterns = [
     url(r'^$', home),
+    url(r'^inscricao/$', subscribe),
     url(r'^admin/', admin.site.urls),
 ]
